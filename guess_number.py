6b70f5db4286d09675decad492056ef35993ecb3
Mario Vera <github.o3idv@passinbox.com>
2024-02-07 18:21:38 -0600

    Refactor code to allow continuous guessing until correct or 'q' entered.

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,21 @@
 number = 10 
  
-print("I'm thinking of a number...") 
-guess = int(input("What number am I thinking of? ")) 
- 
-if guess == number: 
-   print("Congratulations! You guessed the right number.") 
-else: 
-   print(f"Sorry! The number was {number}.") 
+number = 10
+
+print("I'm thinking of a number...")
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+    
+    if guess.lower() == 'q':
+        print(f"The number was {number}.")
+        break
+    
+    if guess.isdigit():
+        guess = int(guess)
+        if guess == number:
+            print("Congratulations! You guessed the right number.")
+            break
+        else:
+            print("Sorry! Incorrect guess. Try again.")
+    else:
+        print("Invalid input. Please enter a number or 'q' to quit.")
