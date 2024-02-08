08231f6ff9d1ecadcc9866076983c687e4fb9bee
Mario Vera <github.o3idv@passinbox.com>
2024-02-07 19:03:50 -0600

    Implemented limited guesses functionality with feedback on remaining guesses.

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,10 @@
-number = 10 
- 
 number = 10
+max_guesses = 3
+remaining_guesses = max_guesses
 
 print("I'm thinking of a number...")
-while True:
-    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+while remaining_guesses > 0:
+    guess = input("What number am I thinking of? ")
     
     if guess.lower() == 'q':
         print(f"The number was {number}.")
@@ -12,10 +12,14 @@ while True:
     
     if guess.isdigit():
         guess = int(guess)
+        remaining_guesses -= 1
         if guess == number:
             print("Congratulations! You guessed the right number.")
             break
+        elif remaining_guesses > 0:
+            print(f"Sorry! Incorrect guess. You have {remaining_guesses} guesses left.")
         else:
-            print("Sorry! Incorrect guess. Try again.")
+            print(f"Sorry! Incorrect guess. No more guesses left. The number was {number}.")
+            break
     else:
         print("Invalid input. Please enter a number or 'q' to quit.")
